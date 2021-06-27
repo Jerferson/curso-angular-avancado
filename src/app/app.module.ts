10c2469fe336cf3b4a7206c8a4650f8659a4fd6c
Jerferson <jerfersongriza@gmail.com>
2021-06-27 12:12:28 -0300

    feat: Criando módulo compartilhado para aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,16 @@ import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { rootRouterConfig } from './app.routes';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { NgBrazil, TextMask } from 'ng-brazil';
 import { CustomFormsModule } from 'ng2-validation';
+import { NavegacaoModule } from './navegacao/navegacao.model';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     CadastroComponent
   ],
@@ -29,6 +24,7 @@ import { CustomFormsModule } from 'ng2-validation';
     ReactiveFormsModule,
     TextMask.TextMaskModule,
     NgBrazil,
+    NavegacaoModule,
     CustomFormsModule,
     [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
   ],
