6f04e9570fbc55d309ffaa57f1fb325f73154103
Jerferson <jerfersongriza@gmail.com>
2021-07-13 19:14:22 -0300

    NgZones com Angular

--- src/app/demos/bar-di-zones/bar.component.ts
+++ src/app/demos/bar-di-zones/bar.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, NgZone, OnInit } from '@angular/core';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
 import { BarFactory, BarService, BebidaService } from './bar.service';
 
@@ -26,12 +26,18 @@ export class BarComponent implements OnInit {
   barBebida1: string;
   barBebida2: string;
   dadosUnidade: string;
+  start: number;
+  end: number;
+
+  public progress: number = 0;
+  public label: string;
 
   constructor(
     private barServices: BarService,
     @Inject('ConfigManualUnidade') private apiConfigManual: BarUnidadeConfig,
     @Inject(BAR_UNIDADE_CONFIG) private apiConfig: BarUnidadeConfig,
-    private bebidaService: BebidaService
+    private bebidaService: BebidaService,
+    private ngZone: NgZone
   ) { }
 
   ngOnInit() {
@@ -42,6 +48,38 @@ export class BarComponent implements OnInit {
     this.dadosUnidade = this.barServices.obterUnidade();
 
     this.barBebida2 = this.bebidaService.obterBebidas();
+  }
+
+  processWithinAngularZone() {
+    this.label = 'dentro';
+    this.progress = 0;
+    this.start = Date.now();
+    this._increaseProgress(() => console.log(`Finalizado por dentro do Angular! Total: ${this.end - this.start}`));
+  }
+
+  processOutsideOfAngularZone() {
+    this.label = 'fora';
+    this.progress = 0;
+    this.start = Date.now();
+    this.ngZone.runOutsideAngular(() => {
+      this._increaseProgress(() => {
+        this.ngZone.run(() => console.log(`Finalizado Fora do Angular! Total: ${this.end - this.start}`));
+      })
+    });
+  }
+
+  _increaseProgress(doneCallback: () => void) {
+    this.progress += 1;
+    console.log(`Progresso atual: ${this.progress}%`);
+
+    if (this.progress < 100) {
+      window.setTimeout(() => {
+        this._increaseProgress(doneCallback)
+        this.end = Date.now();
+      }, 10);
+    } else {
+      doneCallback();
+    }
   }
 
 }
